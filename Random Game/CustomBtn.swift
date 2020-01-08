b64dd0c897ea7a243932ddf3ed696cd4ebdef321
Mariak Achuoth <mariakachuoth@Mariaks-MacBook-Pro.local>
2020-01-07 21:32:01 -0500

    initial project up

---
+++ Random Game/CustomBtn.swift
@@ -0,0 +1,20 @@
+//
+//  CustomBtn.swift
+//  Random Game
+//
+//  Created by Mariak Achuoth on 2020-01-07.
+//  Copyright © 2020 The Future I Dream. All rights reserved.
+//
+
+import UIKit
+
+class CustomBtn: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
